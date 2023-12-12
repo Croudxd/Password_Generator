ae4f0439b915a9f475a586663d592ef19505493a
Croudxd <benjamincroud@gmail.com>
2023-12-12 21:29:12 +0000

    Added Random password generator functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <cstdlib>
+
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +14,23 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+int randomNumber(){
+    int number = rand() % 10;
+    return number;
+}
+
+int* passwordGenerator(int length){
+    int randomPassword[15];
+    if(length < 15){
+        for (int i; i < length; i++){
+            int num = randomNumber();
+            randomPassword[i] = num;
+        }
+    } else {
+           throw std::invalid_argument("Length must be under 15 Characters");
+    }
+
+    return randomPassword;
 }
